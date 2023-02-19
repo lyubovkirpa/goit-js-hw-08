89036be44d0fa501eec630f36042bc9f3e29bacf
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-02-20 00:58:09 +0200

    Сorrecting the page reload solution

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,22 @@
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+import { load } from './storage';
 
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+
+initPage();
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+  localStorage.setItem(LOCAL_STORAGE_KEY, seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+function initPage() {
+  const saveData = load(LOCAL_STORAGE_KEY);
+  if (saveData) {
+    player.setCurrentTime(saveData);
+  }
+}
